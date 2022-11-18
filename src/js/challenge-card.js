23f243dc55d108cf974ca3391858bd9bce6fa3cd
Axel Asp <solidtop.asp@gmail.com>
2022-11-18 14:33:18 +0100

    Changed from innerHTML to createElement

--- src/js/challenge-card.js
+++ src/js/challenge-card.js
@@ -13,35 +13,58 @@ export function createChallengeCard(challenge) {
 
     const item = document.createElement('li');
     item.classList.add('challenge-item');
-    document.querySelector('.challenge-cards').appendChild(item); //Append challenge-item to container
+    document.querySelector('.challenge-cards').appendChild(item);
 
-    item.innerHTML = `
-        <i class="${iconType}"></i>
-        <img class="challenge-image" alt="" src="${challenge.image}">
-        <h3 class="challenge-title">${challenge.title} ${typeTitle}</h3>
-        <ul role="meter" class="rating" aria-label="rating"
-            arial-valuemin="0" aria-valuemax="5" aria-valuenow="${challenge.rating}"
-            aria-valuetext="${challenge.rating} out of 5">
+    const image = document.createElement('img');
+    image.classList.add('challenge-image');
+    image.setAttribute('src', challenge.image);
+    image.setAttribute('alt', 'Cute kitten');
+    item.appendChild(image);
+    
+    const title = document.createElement('h3');
+    title.classList.add('challenge-title');
+    title.textContent = `${challenge.title} ${typeTitle}`;
+    item.appendChild(title);
 
-            <li class="rating-star"></li>
-            <li class="rating-star"></li>
-            <li class="rating-star"></li>
-            <li class="rating-star"></li>
-            <li class="rating-star"></li>
-        </ul>
-        <small class="challenge-meta">${challenge.minParticipants}-${challenge.maxParticipants} participants</small>
-        <p class="challenge-description">
-            ${challenge.description}
-        </p>
-        <button class="button primary">${buttonText}</button>
-    
-    `;
-    
-    //Add active stars
-    const stars = document.querySelectorAll('.challenge-item .rating-star');
-    for (let i = 0; i < challenge.rating; i++) {
-        stars[i].classList.add('active');
-    }
+    const rating = document.createElement('ul');
+    rating.classList.add('rating');
+    rating.setAttribute("role", "meter");
+    rating.setAttribute("aria-label", "rating");
+    rating.setAttribute("arial-valuemin", '0');
+    rating.setAttribute("aria-valuemax", '5');
+    rating.setAttribute("aria-valuenow", challenge.rating);
+    rating.setAttribute("aria-valuetext", challenge.rating + ' out of 5');
+    item.appendChild(rating);
+
+    const stars = [
+        document.createElement('li'), 
+        document.createElement('li'),
+        document.createElement('li'),
+        document.createElement('li'),
+        document.createElement('li'),
+    ];
+    stars.forEach((star, i) => {
+        star.classList.add('rating-star');
+        if (i < challenge.rating) 
+            star.classList.add('active');
+        
+        rating.appendChild(star);
+    });
+
+    const participants = document.createElement('small');
+    participants.classList.add('challenge-meta');
+    participants.textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
+    item.appendChild(participants);
+
+    const description = document.createElement('p');
+    description.classList.add('challenge-description');
+    description.textContent = challenge.description;
+    item.appendChild(description);
+
+    const button = document.createElement('button');
+    button.classList.add('button', 'primary');
+    button.textContent = buttonText;
+    item.appendChild(button);
 
     return item;
 }
