34f5425fdf5275fea22bfccf71cd2badde9f3f98
Axel Asp <solidtop.asp@gmail.com>
2022-11-18 12:59:02 +0100

    Removed placeholder code

--- src/js/challenge-card.js
+++ src/js/challenge-card.js
@@ -1,28 +1,3 @@
-//PLACEHOLDER
-const challenges = [
-    {
-        id: 1,
-        type: "onsite",
-        title: "Project: X of Doom",
-        description: "Try your hardest and succeed. Or fail",
-        minParticipants: 2,
-        maxParticipants: 4,
-        rating: 1,
-        image: "https://placekitten.com/640/480",
-        labels: [
-            "linux",
-            "web",
-            "javascript"
-        ]
-    },
-]
-
-challenges.forEach(challenge => {
-    createChallengeCard(challenge);
-});
-
-//-----------------------------------
-
 export function createChallengeCard(challenge) {
    
     let iconType, buttonText, typeTitle;
