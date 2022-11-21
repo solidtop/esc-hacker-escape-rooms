4bd8ea57c8e8a688b64d1001fe4cbbce87e32f93
Axel Asp <solidtop.asp@gmail.com>
2022-11-21 17:20:01 +0100

    Remove checkbox event listener

--- src/js/filter-type.js
+++ src/js/filter-type.js
@@ -1,32 +1,10 @@
-const checkboxes = document.querySelectorAll('.filter-by-type input[type=checkbox]');
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-        
-        let types = [];
-        const checked = document.querySelectorAll('.filter-by-type input[type=checkbox]:checked');
-        checked.forEach(item => {
-            types.push(item.value);
-        });
+export function filterByType(data) {
+    const types = [];
+    const checked = document.querySelectorAll('.filter-by-type input[type=checkbox]:checked');
+    checked.forEach(item => {
+        types.push(item.value);
+    });
 
-        const filteredArray = filterByType(challenges, types);
-        updateCards(filteredArray);
-    });    
-});
-
-function filterByType(data, types) {
     return data.filter(item => types.some(type => item.type.includes(type)));
 }
 
-function updateCards(data) {
-
-    document.querySelector('.challenge-list').remove(); //Remove existing cards
-
-    //Add new cards
-    const list = document.createElement('ul');
-    list.classList.add('challenge-list');
-    document.body.appendChild(list);
-
-    data.forEach(item => {
-        createChallengeCard(item);
-    });
-}
