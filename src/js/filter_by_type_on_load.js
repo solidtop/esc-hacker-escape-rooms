40e64bad14d85b5f06491e13f152e58a77aa7b7f
David Lidebrandt <david.lidebrandt@gmail.com>
2022-11-21 12:23:08 +0100

    Basic outline for filtering cards via query params on load

---
+++ src/js/filter_by_type_on_load.js
@@ -0,0 +1,14 @@
+function getQueryParams() {
+  const params = window.location.search;
+  const paramSearch = new URLSearchParams(params);
+  const type = paramSearch.get("type");
+  console.log(type)
+
+  //Call function to get API data
+  
+  // if type === online || type === onsite Call function for filtering cards via type
+
+  //Call function for rendering cards
+}
+
+getQueryParams();
