c3901ca8511a328f633c167c522796a129c25968
Marta Anna Todorovic <martaannatodorovic@gmail,com>
2022-11-23 16:38:29 +0100

    Code cleanup

--- src/js/sendReservation.js
+++ src/js/sendReservation.js
@@ -1,16 +1,16 @@
-export default async function reserveTime(availableTimes) {
+export default async function reserveTime(challengeID, requestedName, requestedEmail, requestedDate, requestedTime, requestedParticipants) {
     const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-        challenge: data.id,
-        name: "Customer Name",
-        email: "name@example.com",
-        date: "2022-12-12",
-        time: "18:30",
-        participants: 5,
+        challenge: challengeID,
+        name: requestedName,
+        email: requestedEmail,
+        date: requestedDate,
+        time: requestedTime,
+        participants: requestedParticipants,
     }),
 });
 
