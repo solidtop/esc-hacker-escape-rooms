2896eddbb70551ef4facdb8c9aed0a5546d292f2
David Lidebrandt <david.lidebrandt@gmail.com>
2022-11-28 07:44:11 +0100

    Rename JS files

--- src/js/popularChallenges.js
+++ src/js/popularChallenges.js
@@ -1,6 +1,6 @@
 import loadData from "./loadData.js";
 import { createChallengeCard } from "./challenge-card.js";
-import renderChallenges from "./render_challenges.js";
-import filterPopularChallenges from "./filter_popular_challenges.js";
+import renderChallenges from "./renderChallenges.js";
+import filterPopularChallenges from "./filterPopularChallenges.js";
 
 renderChallenges(loadData, filterPopularChallenges, createChallengeCard, document.querySelector(".challenge-list"));
