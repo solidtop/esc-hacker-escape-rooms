81c28a36d10039b811185001c8487b37e1a87839
Axel Asp <solidtop.asp@gmail.com>
2022-11-29 09:58:25 +0100

    resolve conflict #2

---
+++ src/js/challenge-card.js
@@ -0,0 +1,73 @@
+export function createChallengeCard(challenge) {
+   
+    let iconType, buttonText, typeTitle;
+    if (challenge.type === 'online') {
+        iconType = 'fa-solid fa-laptop';
+        buttonText = 'Take challenge online';
+        typeTitle = '(online)';
+    } else {
+        iconType = 'fa-sharp fa-solid fa-house';
+        buttonText = 'Book this room';
+        typeTitle = '(on-site)';
+    }
+
+    const item = document.createElement('li');
+    item.classList.add('challenge-item');
+
+    const icon = document.createElement('i');
+    icon.classList.add('challenge-icon');
+    icon.innerHTML = `<i class="${iconType}"></i>`;
+    item.appendChild(icon);
+
+    const image = document.createElement('img');
+    image.classList.add('challenge-image');
+    image.setAttribute('src', challenge.image);
+    image.setAttribute('alt', 'Cute kitten');
+    item.appendChild(image);
+    
+    const title = document.createElement('h3');
+    title.classList.add('challenge-title');
+    title.textContent = `${challenge.title} ${typeTitle}`;
+    item.appendChild(title);
+
+    const rating = document.createElement('ul');
+    rating.classList.add('rating');
+    rating.setAttribute("role", "meter");
+    rating.setAttribute("aria-label", "rating");
+    rating.setAttribute("arial-valuemin", '0');
+    rating.setAttribute("aria-valuemax", '5');
+    rating.setAttribute("aria-valuenow", challenge.rating);
+    rating.setAttribute("aria-valuetext", challenge.rating + ' out of 5');
+    item.appendChild(rating);
+
+    for (let i = 0; i < 5; i++) {
+        let star = document.createElement('li');
+        star.classList.add('rating-star');
+        if (i < Math.floor(challenge.rating)) 
+            star.classList.add('active');
+        
+        rating.appendChild(star);
+    }
+
+    const participants = document.createElement('small');
+    participants.classList.add('challenge-meta');
+    participants.textContent = `${challenge.minParticipants}-${challenge.maxParticipants} participants`;
+    item.appendChild(participants);
+
+    const description = document.createElement('p');
+    description.classList.add('challenge-description');
+    description.textContent = challenge.description;
+    item.appendChild(description);
+
+    const currentChallengeObject = document.createElement("input");
+    currentChallengeObject.setAttribute("hidden", "true");
+    currentChallengeObject.setAttribute("value", JSON.stringify(challenge));
+
+    const button = document.createElement('button');
+    button.classList.add('button', 'primary');
+    button.textContent = buttonText;
+    button.append(currentChallengeObject);
+    item.appendChild(button);
+
+    return item;
+}
