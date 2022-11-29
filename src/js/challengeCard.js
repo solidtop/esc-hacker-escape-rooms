2d802fbdb268b6fdb18b8c9638ff8d01e65b0969
Axel Asp <solidtop.asp@gmail.com>
2022-11-29 11:49:13 +0100

    trying to resolve conflict

--- src/js/challengeCard.js
+++ src/js/challengeCard.js
@@ -59,7 +59,7 @@ export default function createChallengeCard(challenge) {
     description.textContent = challenge.description;
     item.appendChild(description);
 
-    const currentChallengeObject = document.createElement("input");
+    const currentChallengeObject = document.createElement("input"); //TEST TEST
     currentChallengeObject.setAttribute("hidden", "true");
     currentChallengeObject.setAttribute("value", JSON.stringify(challenge));
 
