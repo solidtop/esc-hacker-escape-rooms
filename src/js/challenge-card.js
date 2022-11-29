6ed7d8fefcb3c461fbb2c065a3f75111ae01438c
Axel Asp <solidtop.asp@gmail.com>
2022-11-29 10:32:50 +0100

    Resolve conflict 3

--- src/js/challenge-card.js
+++ src/js/challenge-card.js
@@ -1,12 +1,12 @@
-export function createChallengeCard(challenge) {
+export default function createChallengeCard(challenge) {
    
-    let iconType, buttonText, typeTitle;
+    let icon, buttonText, typeTitle;
     if (challenge.type === 'online') {
-        iconType = 'fa-solid fa-laptop';
+        icon = 'fa-solid fa-laptop';
         buttonText = 'Take challenge online';
         typeTitle = '(online)';
     } else {
-        iconType = 'fa-sharp fa-solid fa-house';
+        icon = 'fa-sharp fa-solid fa-house';
         buttonText = 'Book this room';
         typeTitle = '(on-site)';
     }
@@ -14,15 +14,14 @@ export function createChallengeCard(challenge) {
     const item = document.createElement('li');
     item.classList.add('challenge-item');
 
-    const icon = document.createElement('i');
-    icon.classList.add('challenge-icon');
-    icon.innerHTML = `<i class="${iconType}"></i>`;
-    item.appendChild(icon);
+    const i = document.createElement('i');
+    i.setAttribute('class', icon);
+    item.appendChild(i);
 
     const image = document.createElement('img');
     image.classList.add('challenge-image');
-    image.setAttribute('src', challenge.image);
-    image.setAttribute('alt', 'Cute kitten');
+    image.src = challenge.image;
+    image.alt = 'challenge image';
     item.appendChild(image);
     
     const title = document.createElement('h3');
@@ -30,23 +29,24 @@ export function createChallengeCard(challenge) {
     title.textContent = `${challenge.title} ${typeTitle}`;
     item.appendChild(title);
 
-    const rating = document.createElement('ul');
-    rating.classList.add('rating');
-    rating.setAttribute("role", "meter");
-    rating.setAttribute("aria-label", "rating");
-    rating.setAttribute("arial-valuemin", '0');
-    rating.setAttribute("aria-valuemax", '5');
-    rating.setAttribute("aria-valuenow", challenge.rating);
-    rating.setAttribute("aria-valuetext", challenge.rating + ' out of 5');
-    item.appendChild(rating);
+    const ratingList = document.createElement('ul');
+    ratingList.classList.add('rating');
+    ratingList.setAttribute("role", "meter");
+    ratingList.setAttribute("aria-label", "rating");
+    ratingList.setAttribute("arial-valuemin", '0');
+    ratingList.setAttribute("aria-valuemax", '5');
+    ratingList.setAttribute("aria-valuenow", challenge.rating);
+    ratingList.setAttribute("aria-valuetext", challenge.rating + ' out of 5');
+    item.appendChild(ratingList);
 
+    const rating = Math.floor(challenge.rating);
     for (let i = 0; i < 5; i++) {
         let star = document.createElement('li');
         star.classList.add('rating-star');
-        if (i < Math.floor(challenge.rating)) 
+        if (i < rating) 
             star.classList.add('active');
         
-        rating.appendChild(star);
+        ratingList.appendChild(star);
     }
 
     const participants = document.createElement('small');
