08d75af3f7b582ca05f5dfe60299051d51db2f78
jonathanholmgren <jonathan.holmgren@hotmail.com>
2022-12-01 17:03:39 +0100

    [removed]

--- src/js/loadData.js
+++
@@ -1,5 +0,0 @@
-export default async function loadData() {
-    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
-    const data = await res.json();
-    return data.challenges;
-};
