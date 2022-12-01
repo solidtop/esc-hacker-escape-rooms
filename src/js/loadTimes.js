365ff83ce7cd27a2a5e18e048a208622e905b9fb
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2022-12-01 13:41:08 +0100

    Change in loadTimes to return data not data.slots

--- src/js/loadTimes.js
+++ src/js/loadTimes.js
@@ -1,10 +1,7 @@
 export default async function loadTimes(challengeID, selectedDate) {
     const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=' + selectedDate + '&challenge='+ challengeID);
     const data = await res.json();
-    data.slots.forEach(slot => {
-        console.log(slot)
-    });
-    return data.slots;
+    return data;
 };
 
 
