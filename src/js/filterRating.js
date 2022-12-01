50ca504876dc30423cd63e5a21af733c7c61bed5
jonathanholmgren <jonathan.holmgren@hotmail.com>
2022-12-01 16:21:54 +0100

    [New update, with export]

---
+++ src/js/filterRating.js
@@ -0,0 +1,59 @@
+
+export function handleStarInput(data) {
+
+  const starsFrom = document.querySelectorAll(".starsFrom li");
+  const starsTo = document.querySelectorAll(".starsTo li");
+  
+  let activeIdx = null;
+
+  starsFrom.forEach((starFrom, clickIdx) => {
+    starFrom.addEventListener("click", (e) => {
+      if (activeIdx === clickIdx) {
+        e.target.classList.remove("active");
+        activeIdx = clickIdx - 1;
+      } else {
+        e.target.classList.add("active");
+        activeIdx = clickIdx;
+      }
+  
+      starsFrom.forEach((otherStar, otherIdx) => {
+        if (otherIdx < clickIdx) {
+          otherStar.classList.add("active");
+        } else if (otherIdx > clickIdx) {
+          otherStar.classList.remove("active");
+        }
+      });
+    });
+  });
+
+  starsTo.forEach((starTo, clickIdx) => {
+    starTo.addEventListener("click", (e) => {
+      if (activeIdx === clickIdx) {
+        e.target.classList.remove("active");
+        activeIdx = clickIdx - 1;
+      } else {
+        e.target.classList.add("active");
+        activeIdx = clickIdx;
+      }
+
+      starsTo.forEach((otherStar, otherIdx) => {
+        if (otherIdx < clickIdx) {
+          otherStar.classList.add("active");
+        } else if (otherIdx > clickIdx) {
+          otherStar.classList.remove("active");
+        }
+      });
+      console.log(filterByRating(data));
+    });
+  });
+}
+
+export function filterByRating(data){
+  const valueFrom = document.querySelectorAll('#starFrom .active');
+  const valueTo = document.querySelectorAll('#starTo .active');
+
+  return data.filter(challenge => {
+    return challenge.rating >= valueFrom.length && challenge.rating <= valueTo.length
+  });
+}
+
