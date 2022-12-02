1bfd921d71475ab08744b66911783532e2b4e198
David Lidebrandt <david.lidebrandt@gmail.com>
2022-12-02 12:40:32 +0100

    Show text when no matching challenges

--- src/js/renderChallenges.js
+++ src/js/renderChallenges.js
@@ -9,17 +9,24 @@ async function renderChallenges(data, filterFunction=function(data){return data}
 
   async function filterAndUpdateCards(data, renderFunction, challengeContainer, ...filterFunctions) {
     let challenges = await data;
-   
+    challengeContainer.innerHTML = "";
+    challengeContainer.nextElementSibling.classList.add("hidden");
     filterFunctions.forEach((filter)=> {
       challenges = filter(challenges);
       console.log(challenges)
     });
-    challengeContainer.innerHTML = "";
-    challenges.forEach((challenge)=> {
-      const currentCard = renderFunction(challenge);
-      console.log(currentCard)
-      challengeContainer.append(currentCard);
-    })
+    if(challenges.length === 0) {
+      console.log("h")
+      console.log(challengeContainer.nextSibling)
+      challengeContainer.nextElementSibling.classList.remove("hidden");
+    } else {
+      challenges.forEach((challenge)=> {
+        const currentCard = renderFunction(challenge);
+        console.log(currentCard)
+        challengeContainer.append(currentCard);
+      });
+    }
+   
   }
 
   export {renderChallenges, filterAndUpdateCards}
