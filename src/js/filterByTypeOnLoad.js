706fdb2cd5246ff3f19898f7199c1351d0d105c4
David Lidebrandt <david.lidebrandt@gmail.com>
2022-12-13 10:47:49 +0100

    Add remaining queryparam filters

--- src/js/filterByTypeOnLoad.js
+++ src/js/filterByTypeOnLoad.js
@@ -9,6 +9,7 @@ function getQueryParams() {
   const queryParams = {};
   const params = window.location.search;
   const paramSearch = new URLSearchParams(params);
+  
   queryParams.type = paramSearch.get("type");
   queryParams.minRating = paramSearch.get("min-rating");
   queryParams.maxRating = paramSearch.get("max-rating");
@@ -17,6 +18,7 @@ function getQueryParams() {
   if(queryParams.tags != null) {
     queryParams.tags = queryParams.tags.split(",");
   }
+  
   return queryParams;
 }
 
@@ -27,13 +29,37 @@ async function renderChallenges(queryParams, container, data, filterFunction, re
   if(params.type === "online") {
     challenges = filterFunction(challenges, "online");
   }
+  
   else if(params.type === "onsite") {
     challenges = filterFunction(challenges, "onsite");
   }
-  if(params.minRating != null && params.minRating > 0 && params.minRating < 5) {
+  
+  if(params.minRating != null && params.minRating > 0 && params.minRating <= 5) {
     const starsFrom = document.querySelectorAll(".starsFrom li");
     starsFrom[parseInt(params.minRating)-1].click();
   }
+  
+  if(params.maxRating != null && params.maxRating >= 0 && params.maxRating < 5) {
+    const starsTo = document.querySelectorAll(".starsTo li");
+    starsTo[parseInt(params.maxRating)-1].click();
+  }
+  
+  if(params.text != null) {
+    const inputValue = document.getElementById("text-filter");
+    inputValue.setAttribute("value", params.text);
+  }
+
+  if(params.tags != null && params.tags.length > 0) {
+    const tagButtons = document.querySelectorAll(".btn");
+    params.tags.forEach((param)=> {
+      tagButtons.forEach((tag)=> {
+        if(param === tag.innerText) {
+          tag.click();
+        }
+      });
+    });
+  }
+  
   challenges.forEach((challenge) => {
     const card = renderFunction(challenge);
     container.append(card);
